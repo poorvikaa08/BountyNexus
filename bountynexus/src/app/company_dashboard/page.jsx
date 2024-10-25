569010bf65d51b634de34087f7f8977458a48963
prakruthiprasad20 <prakruthiprasad19@gmail.com>
2024-10-25 12:11:30 +0530

    add changes

--- bountynexus/src/app/company_dashboard/page.jsx
+++ bountynexus/src/app/company_dashboard/page.jsx
@@ -57,14 +57,37 @@
 'use client';
 
 import Image from "next/image";
+import { useSession } from "next-auth/react";
 import React from 'react';
-import styles from "./dashboard.css";
+import styles from "./company_dashboard.css";
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { signOut } from "next-auth/react";
 
 // import logo1.png from './logo1.png.jpg'; 
 
 const Dashboard = () => {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  // useEffect(() => {
+  //   // If there's no session and the session status is not loading, redirect to login
+  //   if (status === "unauthenticated") {
+  //     router.push("/login");  // Redirect to login if not logged in
+  //     console.log(status);
+  //   }
+  //   // const loggedIn = Cookies.get("loggedin");
+  //   // if (!loggedIn) {
+  //   //   router.push("/login");
+  //   // }
+  // }, [session, status, router]);
+
+  // const logout = () => {
+  //   Cookies.remove("loggedin");
+  //   router.push("/");
+  // };
   return (
     <div className="dashboard">
       <div className="icon-container">
@@ -90,22 +113,17 @@ const Dashboard = () => {
 
 {/* Main content area */}
 <div className="main-content">
-        <h1>Hello, Bug Busters</h1>
-     <p>Track down bugs, rack up rewards, and show the world what you’ve got!</p>
-       <p>Customize your profile by clicking the icon. Your adventure awaits!</p> 
-
+        <h1>Hello</h1>
+        <span>{session?.user?.name}</span>
+        <h1>Welcome!</h1>
+     {/* <p>Track down bugs, rack up rewards, and show the world what you’ve got!</p>
+       <p>Customize your profile by clicking the icon. Your adventure awaits!</p>  */}
+      <button type="submit" onClick={() => signOut()}>Log Out</button>
          {/* The text you want in the free space */}
       </div>
 
     </div>
-
-
-
-
- 
-
-
- );
+   );
  };
 
 export default Dashboard;
